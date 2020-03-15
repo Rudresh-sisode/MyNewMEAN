c5aa373052f4790b2d280842ba34d9a64629d841
Rudresh Sisode <rudranrajput@gmail.com>
2020-03-15 08:49:12 +0530

    MEAN project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './post/post-create/post-component';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
+  //all the component and pipe goes here
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
+  //all the module will be declared here
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  //all servicess will be declared here.
   providers: [],
+  //only first component will be declared here.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
